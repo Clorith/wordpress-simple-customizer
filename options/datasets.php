90c6a0aba90c92d5dc69c6a291a1ca86c5d0201e
Marius <funman@gmail.com>
2013-08-16 12:15:07 +0200

    Bumping to SVN version

--- options/datasets.php
+++ options/datasets.php
@@ -34,6 +34,8 @@
                         <td>' . $theme->Author . '</td>
                         <td>
                             <a href="?page=simple-customize&tab=datasets&clear=' . $themename . '">' . __( 'Delete stylings', 'simple-customize-plugin' ) . '</a>
+                             |
+                            <a href="?page=simple-customize&tab=datasets&export=' . $themename . '">' . __( 'Export customizations', 'simple-customize-plugin' ) . '</a>
                         </td>
                     </tr>
                 ';
@@ -42,3 +44,63 @@
         </tbody>
     </table>
 </form>
+
+<?php
+    if ( isset( $_GET['export'] ) )
+    {
+        $export = array(
+            'theme'      => $_GET['export'],
+            'categories' => array(),
+            'options'    => array(),
+            'fonts'      => array()
+        );
+
+        if ( isset( $categories[$_GET['export']] ) )
+        {
+            foreach( $categories[$_GET['export']] AS $category )
+            {
+                $export['categories'][] = $category;
+            }
+        }
+
+        if ( isset( $options[$_GET['export']] ) )
+        {
+            $theme_mods = get_theme_mods();
+
+            foreach( $options[$_GET['export']] AS $option )
+            {
+                $mod = sanitize_title( $option['label'] );
+                $option['value'] = $theme_mods[$mod];
+
+                $export['options'][] = $option;
+            }
+        }
+
+        if ( isset( $fonts[$_GET['export']] ) )
+        {
+            foreach( $fonts[$_GET['export']] AS $fnum => $font )
+            {
+                $export['fonts'][] = $font;
+            }
+        }
+?>
+
+    <h3><?php _e( 'Export customization', 'simple-customize-plugin' ); ?></h3>
+    <textarea name="simple-customize-export" style="width: 100%; height: 75px;"><?php echo base64_encode( serialize( $export ) ); ?></textarea>
+
+<?php
+        _e( 'Copy the text above, it represents your themes customizations, categories and fonts', 'simple-customize-plugin' );
+    }
+    else {
+?>
+
+<form action="" method="post">
+    <h3><?php _e( 'Import customizations', 'simple-customize-plugin' ); ?></h3>
+
+    <textarea name="simple-customize-import" style="width: 100%; height: 75px;" placeholder="Put your customization string here"></textarea>
+    <?php submit_button( __( 'Add customization', 'simple-customize-plugin' ) ); ?>
+</form>
+
+<?php
+    }
+?>
